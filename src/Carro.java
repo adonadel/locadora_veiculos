3c9e6ab059050f5b2fd7e49162acc7b1bb2e245d
ERIC DAGOSTIM DO NASCIMENTO <eric.nascimento@alunos.sc.senac.br>
2022-10-10 21:24:08 -0300

    Classe veiculo e Carro criadas, carro extend veiculo

---
+++ src/Carro.java
@@ -0,0 +1,10 @@
+public class Carro extends Veiculo{
+    private int numeroPortas;
+    private float litragemPortaMalas;
+    private float capacidadeTanque;
+    private float kmPorLitro; //média de km por litro
+    private int numeroEixos;
+    private int numeroRodas;
+
+
+}
---
+++ src/Veiculo.java
@@ -0,0 +1,18 @@
+public class Veiculo<localdate> {
+
+    private long id;
+    private float peso;
+    private localdate dataFabricacao;
+    private int numeroSerie;
+    private double valorFipe;
+    private double valorCompra;
+    private int hodometro;
+    private String descricao;
+    private String tipoCombustivel;
+    private String placa;
+    private localdate ultimaRevisao;
+    private float TrocaOleokm; //ultima troca em km (50.000 foi trocado o oleo)
+
+
+
+}
