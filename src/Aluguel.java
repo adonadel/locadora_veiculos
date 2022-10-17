dea231f9da02966915f2ee04c2839e3568816247
Alexandre Donadel Fernandes <xanded.fer2@gmail.com>
2022-10-17 20:32:38 -0300

    Começo da classe aluguel

---
+++ src/Aluguel.java
@@ -0,0 +1,86 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Aluguel {
+    private long id;
+    private BigDecimal valorEstimado;
+    private BigDecimal valorFinal;
+    private LocalDate dataAluguel;
+    private LocalDate dataDevolucao;
+    private long hodometroInicial;
+    private long hodometroFinal;
+    private Pessoa pessoa;
+    private Veiculo veiculo;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValorEstimado() {
+        return valorEstimado;
+    }
+
+    public void setValorEstimado(BigDecimal valorEstimado) {
+        this.valorEstimado = valorEstimado;
+    }
+
+    public BigDecimal getValorFinal() {
+        return valorFinal;
+    }
+
+    public void setValorFinal(BigDecimal valorFinal) {
+        this.valorFinal = valorFinal;
+    }
+
+    public LocalDate getDataAluguel() {
+        return dataAluguel;
+    }
+
+    public void setDataAluguel(LocalDate dataAluguel) {
+        this.dataAluguel = dataAluguel;
+    }
+
+    public LocalDate getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(LocalDate dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public long getHodometroInicial() {
+        return hodometroInicial;
+    }
+
+    public void setHodometroInicial(long hodometroInicial) {
+        this.hodometroInicial = hodometroInicial;
+    }
+
+    public long getHodometroFinal() {
+        return hodometroFinal;
+    }
+
+    public void setHodometroFinal(long hodometroFinal) {
+        this.hodometroFinal = hodometroFinal;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+        this.veiculo = veiculo;
+    }
+}
