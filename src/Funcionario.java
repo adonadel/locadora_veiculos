a893d59dcb6a21c105b8207f9e83c66cdac813bd
BIANCA FRANCELINO PIZZOLO <bianca.pizzolo@alunos.sc.senac.br>
2022-10-19 20:01:30 -0300

    Criano PessoaBuilder

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -3,4 +3,32 @@ import java.math.BigDecimal;
 public class Funcionario extends Pessoa{
     private BigDecimal salarioBruto, salarioLiquido;
     private String nome;
+
+    public BigDecimal getSalarioBruto() {
+        return salarioBruto;
+    }
+
+    public void setSalarioBruto(BigDecimal salarioBruto) {
+        this.salarioBruto = salarioBruto;
+    }
+
+    public BigDecimal getSalarioLiquido() {
+        return salarioLiquido;
+    }
+
+    public void setSalarioLiquido(BigDecimal salarioLiquido) {
+        this.salarioLiquido = salarioLiquido;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Funcionario(){
+        setTipo("Funcionario");
+    }
 }
